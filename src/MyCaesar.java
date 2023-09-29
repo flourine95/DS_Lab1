6ec41573ed58a416cad9afc4993f43b512b7f65d
Nguyễn Phi Long <xanh3248@gmail.com>
2023-09-30 01:52:07 +0700

    - Done Task 1 & Task 2 - Update Task 3

--- src/MyCaesar.java
+++ src/MyCaesar.java
@@ -1,10 +1,8 @@
 public class MyCaesar {
 
-    public static final char[] ALPHABET = {
-            'A', 'B', 'C', 'D', 'E', 'F',
-            'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
-            'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-    };
+    public static final char[] ALPHABET = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    public static final char[] NUMBERS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
     private int n;// shift steps (right shift)
 
     public MyCaesar(int shiftSteps) {
@@ -12,15 +10,18 @@ public class MyCaesar {
     }
 
     public char encryptChar(char c) {
-        if (new String(ALPHABET).indexOf(c) < 0) {
+        if (new String(ALPHABET).indexOf(c) < 0 && new String(NUMBERS).indexOf(c) < 0) {
             return c;
         }
-        int index = c - ALPHABET[0];
-        int indexShift = (index + n) % ALPHABET.length;
-        if (indexShift < 0) {
-            indexShift += ALPHABET.length;
+        int index, indexShift;
+        if (Character.isDigit(c)) {
+            index = c - NUMBERS[0];
+            indexShift = (index + n) % NUMBERS.length < 0 ? (index + n) % NUMBERS.length + NUMBERS.length : (index + n) % NUMBERS.length;
+        } else {
+            index = c - ALPHABET[0];
+            indexShift = (index + n) % ALPHABET.length < 0 ? (index + n) % ALPHABET.length + ALPHABET.length : (index + n) % ALPHABET.length;
         }
-        return ALPHABET[indexShift];
+        return Character.isDigit(c) ? NUMBERS[indexShift] : ALPHABET[indexShift];
     }
 
     public String encrypt(String input) {
@@ -32,15 +33,18 @@ public class MyCaesar {
     }
 
     public char decryptChar(char c) {
-        if (new String(ALPHABET).indexOf(c) < 0) {
+        if (new String(ALPHABET).indexOf(c) < 0 && new String(NUMBERS).indexOf(c) < 0) {
             return c;
         }
-        int index = c - ALPHABET[0];
-        int indexShift = (index - n) % ALPHABET.length;
-        if (indexShift < 0) {
-            indexShift += ALPHABET.length;
+        int index, indexShift;
+        if (Character.isDigit(c)) {
+            index = c - NUMBERS[0];
+            indexShift = (index - n) % NUMBERS.length < 0 ? (index - n) % NUMBERS.length + NUMBERS.length : (index - n) % NUMBERS.length;
+        } else {
+            index = c - ALPHABET[0];
+            indexShift = (index - n) % ALPHABET.length < 0 ? (index - n) % ALPHABET.length + ALPHABET.length : (index - n) % ALPHABET.length;
         }
-        return ALPHABET[indexShift];
+        return Character.isDigit(c) ? NUMBERS[indexShift] : ALPHABET[indexShift];
     }
 
     public String decrypt(String input) {
@@ -52,10 +56,10 @@ public class MyCaesar {
     }
 
     public static void main(String[] args) {
-        MyCaesar myCaeser = new MyCaesar(3);
-        System.out.println(myCaeser.encryptChar('A'));
-        System.out.println(myCaeser.decryptChar(myCaeser.encryptChar('A')));
-        System.out.println(myCaeser.encrypt("HELLO WORLD"));
-        System.out.println(myCaeser.decrypt(myCaeser.encrypt("HELLO WORLD")));
+        MyCaesar myCaeser = new MyCaesar(22);
+        System.out.println(myCaeser.encryptChar('2'));
+        System.out.println(myCaeser.decryptChar(myCaeser.encryptChar('2')));
+        System.out.println(myCaeser.encrypt("HELLO WORLD 1234567890"));
+        System.out.println(myCaeser.decrypt(myCaeser.encrypt("HELLO WORLD 1234567890")));
     }
 }
